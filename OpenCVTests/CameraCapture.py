7643a04113657d8dd8e37998ff974a22b09dd093
Rob Prouse <rob@prouse.org>
2014-07-17 15:10:51 -0400

    Refactoring the code a bit

--- OpenCVTests/CameraCapture.py
+++ OpenCVTests/CameraCapture.py
@@ -1,36 +1,52 @@
 import numpy as np
 import cv2
+import cv2.cv as cv
 
-# Capture the video stream
-cap = cv2.VideoCapture(0)
+def detect(img, cascade):
+    rects = cascade.detectMultiScale(img, scaleFactor=1.3, minNeighbors=4, minSize=(30, 30), flags = cv.CV_HAAR_SCALE_IMAGE)
+    if len(rects) == 0:
+        return []
+    rects[:,2:] += rects[:,:2]
+    return rects
 
-# Load the Haar-cascade classifiers for face and eyes
-face_cascade = cv2.CascadeClassifier('data/haarcascades/haarcascade_frontalface_alt.xml')
-eye_cascade = cv2.CascadeClassifier('data/haarcascades/haarcascade_mcs_eyepair_big.xml')
+def draw_rects(img, rects, color):
+    for x1, y1, x2, y2 in rects:
+        cv2.rectangle(img, (x1, y1), (x2, y2), color, 2)
+        
+if __name__ == '__main__':
+    # Capture the video stream
+    cap = cv2.VideoCapture(0)
 
-while(True):
-    # Capture frame-by-frame
-    ret, frame = cap.read()
+    # Load the Haar-cascade classifiers for face and eyes
+    face_cascade = cv2.CascadeClassifier('data/haarcascades/haarcascade_frontalface_alt.xml')
+    eye_cascade = cv2.CascadeClassifier('data/haarcascades/haarcascade_mcs_eyepair_big.xml')
 
-    # Convert to greyscale for face detection
-    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    while(True):
+        # Capture frame-by-frame
+        ret, frame = cap.read()
 
-    # Find the faces
-    faces = face_cascade.detectMultiScale(gray, 1.3, 5)
-    for (x,y,w,h) in faces:
-        cv2.rectangle(frame,(x,y),(x+w,y+h),(255,0,0),2)
-        roi_gray = gray[y:y+h, x:x+w]
-        roi_color = frame[y:y+h, x:x+w]
+        # Convert to greyscale for face detection
+        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+        gray = cv2.equalizeHist(gray)
 
-        eyes = eye_cascade.detectMultiScale(roi_gray)
-        for (ex,ey,ew,eh) in eyes:
-            cv2.rectangle(roi_color,(ex,ey),(ex+ew,ey+eh),(0,255,0),2)
+        # Find the faces
+        faces = detect(gray, face_cascade)
+        vis = frame.copy()
+        draw_rects(vis, faces, (255,0,0))
 
-    # Display the resulting frame
-    cv2.imshow('frame',frame)
-    if cv2.waitKey(1) & 0xFF == ord('q'):
-        break
+        for (x,y,w,h) in faces:
+            roi_gray = gray[y:y+h, x:x+w]
+            roi_color = vis[y:y+h, x:x+w]
 
-# When everything done, release the capture
-cap.release()
-cv2.destroyAllWindows()
+            eyes = detect(roi_gray.copy(), eye_cascade)
+            draw_rects(roi_color, eyes, (0,255,0))
+
+        # Display the resulting frame
+        cv2.imshow('frame',vis)
+        
+        if 0xFF & cv2.waitKey(5) == 27:
+            break
+
+    # When everything done, release the capture
+    cap.release()
+    cv2.destroyAllWindows()
